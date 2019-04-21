8750b3a29670072f30aec7260cbb3b64f603c382
rtwillett <ryan.willett@gmail.com>
2019-04-21 03:11:56 -0400

    Added CSS style

--- server.R
+++ server.R
@@ -12,7 +12,7 @@ shinyServer(function(input, output){
   output$volcano <- renderPlot(expr_data %>% 
     mutate(col_flag = ifelse(((l2fc>=log2(s_expr()) | l2fc<=-log2(s_expr())) & neg_pL10 > -1*log10(s_pval())),1,0)) %>% 
     ggplot(aes(x=l2fc, y=neg_pL10, col=as.factor(col_flag))) + t + 
-    geom_point(size=0.2) + #+scale_y_continuous(trans="log10")
+    geom_point(size=0.2) +
     plot_colors +
     #scale_y_continuous(trans = "log2") +
       labs(
@@ -20,30 +20,32 @@ shinyServer(function(input, output){
         y = "-log10(p-value)",
         title = "Expression Differences of Significantly Expressed Genes",
         caption = "Genes of interest appear in red"
-      )
+      ) +
+    guides(col=F) # Removes the legend
   )
-  sigs_hm <- reactive({
-    
-    top_sig_genes <- expr_data %>% 
-      mutate(col_flag = ifelse(((l2fc>=log2(s_expr()) | l2fc<=-log2(s_expr())) & neg_pL10 > -1*log10(s_pval())),1,0)) %>% 
-      filter(col_flag == 1 & !is.na(fold.change)) %>% 
-      select(-col_flag) %>% 
-      arrange(desc(fold.change)) %>% 
-      slice(-101:-(n()-101)) #Return the highest 100 and lowest 100 gene expression changes
-    
-    hm_d <- data %>% filter(primary == input1 | primary == input2) %>% select(-primary) %>% t() %>% as.data.frame()
-    colnames(hm_d) <- as.character(unlist(hm_d[1, ]))
-    hm_d <- hm_d[-1, ]
-    hm_d <- hm_d %>% rownames_to_column("genes")
-    
-    sigs <- top_sig_genes[, 1, drop = F]
-    sigs_join <- merge(sigs, hm_d, by = "genes")
-    sigs_join <- sigs_join %>% column_to_rownames("genes") %>% as.matrix()
-    sigs_join <- apply(sigs_join, 2, as.numeric) # Converting character matrix to numeric matrix for heatmap calculation
-  })
+  
+  # sigs_hm <- reactive({
+  #   
+  #   top_sig_genes <- expr_data %>% 
+  #     mutate(col_flag = ifelse(((l2fc>=log2(s_expr()) | l2fc<=-log2(s_expr())) & neg_pL10 > -1*log10(s_pval())),1,0)) %>% 
+  #     filter(col_flag == 1 & !is.na(fold.change)) %>% 
+  #     select(-col_flag) %>% 
+  #     arrange(desc(fold.change)) %>% 
+  #     slice(-101:-(n()-101)) #Return the highest 100 and lowest 100 gene expression changes
+  #   
+  #   hm_d <- data %>% filter(primary == input1 | primary == input2) %>% select(-primary) %>% t() %>% as.data.frame()
+  #   colnames(hm_d) <- as.character(unlist(hm_d[1, ]))
+  #   hm_d <- hm_d[-1, ]
+  #   hm_d <- hm_d %>% rownames_to_column("genes")
+  #   
+  #   sigs <- top_sig_genes[, 1, drop = F]
+  #   sigs_join <- merge(sigs, hm_d, by = "genes")
+  #   sigs_join <- sigs_join %>% column_to_rownames("genes") %>% as.matrix()
+  #   sigs_join <- apply(sigs_join, 2, as.numeric) # Converting character matrix to numeric matrix for heatmap calculation
+  # })
   
   output$heatmap <- renderD3heatmap({
-    d3heatmap(sigs_hm(), scale = "row", colors="RdYlGn")
+    d3heatmap(sigs_join, scale = "row", colors="RdYlGn")
   }) 
   
   sig_genes <- reactive({
--- ui.R
+++ ui.R
@@ -1,13 +1,15 @@
 fluidPage(
-  titlePanel("Yeast Transcriptomics Browser"),
+  includeCSS("style.css"),
+  headerPanel(p(id="top", "Yeast Transcriptomics Browser")),
   sidebarLayout(sidebarPanel(
     selectizeInput(
       inputId = "selected",
       label = "Select Genomics Dataset",
       choices = choices
     ),
-    sliderInput("slider_pval", label=h3("P-Value Threshold"), min=0.0001, max=0.1, value = 0.05), 
-    sliderInput("slider_expression", label=h3("Fold Gene Expression Change Threshold"), min=1, max=10, value = 2), 
+    hr(), 
+    sliderInput("slider_pval", label=h3("P-Value Threshold"), min=0.00001, max=0.05, value = 0.05), 
+    sliderInput("slider_expression", label=h3("Fold Gene Expression Change Threshold (Up/Down)"), min=1, max=10, value = 2), 
     hr(), 
     downloadButton("downloadSet", "Download Gene List")
   ), 
@@ -17,8 +19,8 @@ fluidPage(
     tabPanel("Gene Expression",
              fluidRow(column(
                12, plotOutput("volcano")
-             )),
-             HTML("<br><br><br>"),
+             ))),
+    tabPanel("Cluster Analysis", 
              fluidRow(column(
                12, d3heatmapOutput("heatmap")
              ))),
